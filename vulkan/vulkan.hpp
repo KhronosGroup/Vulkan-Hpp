95984317a282f266b50174e161fbaa20f44e23f5
Markus Tavenrath <matavenrath@nvidia.com>
2016-10-07 14:20:59 +0200

    Update to Vulkan 1.0.30

--- vulkan/vulkan.hpp
+++ vulkan/vulkan.hpp
@@ -40,7 +40,7 @@
 # include <vector>
 #endif /*VULKAN_HPP_DISABLE_ENHANCED_MODE*/
 
-static_assert( VK_HEADER_VERSION ==  29 , "Wrong VK_HEADER_VERSION!" );
+static_assert( VK_HEADER_VERSION ==  30 , "Wrong VK_HEADER_VERSION!" );
 
 // 32-bit vulkan is not typesafe for handles, so don't allow copy constructors on this platform by default.
 // To enable this feature on 32-bit platforms please define VULKAN_HPP_TYPESAFE_CONVERSION

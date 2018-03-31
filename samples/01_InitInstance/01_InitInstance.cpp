bbaa5956c6dce6e67ae81aaa7151b20f8d037a73
Andreas Süßenbach <asuessenbach@nvidia.com>
2018-03-31 10:09:50 +0200

    First samples on using vulkan.hpp: 01_InitInstance, 02_EnumerateDevices, 03_InitDevice, 04_InitCommandBuffer, 05_InitSwapchain, 06_InitDepthBuffer. (#197)

---
+++ samples/01_InitInstance/01_InitInstance.cpp
@@ -0,0 +1,53 @@
+// Copyright(c) 2018, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// VulkanHpp Samples : 01_InitInstance
+//                     Create and destroy a vk::UniqueInstance
+
+#include "vulkan/vulkan.hpp"
+#include <iostream>
+
+static char const* AppName = "01_InitInstance";
+static char const* EngineName = "Vulkan.hpp";
+
+int main(int argc, char *argv[])
+{
+  /* VULKAN_HPP_KEY_START */
+
+  try
+  {
+    // initialize the vk::ApplicationInfo structure
+    vk::ApplicationInfo appInfo(AppName, 1, EngineName, 1, VK_API_VERSION_1_1);
+
+    // create a UniqueInstance
+    vk::UniqueInstance instance = vk::createInstanceUnique(vk::InstanceCreateInfo(vk::InstanceCreateFlags(), &appInfo));
+
+    // Note: No need to explicitly destroy the instance, as the corresponding destroy function is
+    // called by the destructor of the UniqueInstance on leaving this scope.
+  }
+  catch (vk::SystemError err)
+  {
+    std::cout << "vk::SystemError: " << err.what() << std::endl;
+    exit(-1);
+  }
+  catch (...)
+  {
+    std::cout << "unknown error\n";
+    exit(-1);
+  }
+
+  /* VULKAN_HPP_KEY_END */
+
+  return 0;
+}
---
+++ samples/02_EnumerateDevices/02_EnumerateDevices.cpp
@@ -0,0 +1,52 @@
+// Copyright(c) 2018, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// VulkanHpp Samples : 02_EnumerateDevices
+//                     Enumerate physical devices
+
+#include "vulkan/vulkan.hpp"
+#include <iostream>
+
+static char const* AppName = "02_EnumerateDevices";
+static char const* EngineName = "Vulkan.hpp";
+
+int main(int argc, char *argv[])
+{
+  try
+  {
+    vk::ApplicationInfo appInfo(AppName, 1, EngineName, 1, VK_API_VERSION_1_1);
+    vk::UniqueInstance instance = vk::createInstanceUnique(vk::InstanceCreateInfo({}, &appInfo));
+
+    /* VULKAN_HPP_KEY_START */
+
+    // enumerate the physicalDevices
+    std::vector<vk::PhysicalDevice> physicalDevices = instance->enumeratePhysicalDevices();
+
+    // Note: PhysicalDevices are not created, but just enumerated. Therefore, there is nothing like a UniquePhysicalDevice.
+    // A PhysicalDevice is unique by definition, and there's no need to destroy it.
+
+    /* VULKAN_HPP_KEY_END */
+  }
+  catch (vk::SystemError err)
+  {
+    std::cout << "vk::SystemError: " << err.what() << std::endl;
+    exit(-1);
+  }
+  catch (...)
+  {
+    std::cout << "unknown error\n";
+    exit(-1);
+  }
+  return 0;
+}

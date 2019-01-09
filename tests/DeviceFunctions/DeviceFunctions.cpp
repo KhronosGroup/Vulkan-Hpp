a147b0773725ca6427284c1e49605f823e3343f1
Andreas Süßenbach <asuessenbach@nvidia.com>
2019-01-09 11:55:11 +0100

    Cleanup code generation with focus on removing the need to sort all dependencies before writing them.

--- tests/DeviceFunctions/DeviceFunctions.cpp
+++ tests/DeviceFunctions/DeviceFunctions.cpp
@@ -51,6 +51,8 @@ int main(int /*argc*/, char * /*argv[]*/)
     std::vector<vk::UniqueCommandBuffer>::allocator_type vectorAllocator;
     std::vector<vk::UniqueCommandBuffer> commandBuffers = device->allocateCommandBuffersUnique({}, vectorAllocator, vk::DispatchLoaderStatic());
 
+    commandBuffers[0]->begin(nullptr);
+
     std::vector<vk::UniqueHandle<vk::CommandBuffer, vk::DispatchLoaderDynamic>>::allocator_type dynamicVectorAllocator;
     std::vector<vk::UniqueHandle<vk::CommandBuffer, vk::DispatchLoaderDynamic>> dynamicCommandBuffers = device->allocateCommandBuffersUnique({}, dynamicVectorAllocator, vk::DispatchLoaderDynamic());
   }

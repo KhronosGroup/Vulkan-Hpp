50c537eb9190c0f2b552de0630c070b18ada3ec0
Andreas Süßenbach <asuessenbach@nvidia.com>
2023-06-21 09:17:06 +0200

    Update StructureChain.hpp

--- snippets/StructureChain.hpp
+++ snippets/StructureChain.hpp
@@ -127,6 +127,17 @@
       return std::tie( get<T0>(), get<T1>(), get<Ts>()... );
     }
 
+    // assign a complete structure to the StructureChain without modifying the chaining
+    template <typename T = typename std::tuple_element<0, std::tuple<ChainElements...>>::type, size_t Which = 0>
+    StructureChain & assign( const T & rhs ) VULKAN_HPP_NOEXCEPT
+    {
+      T &    lhs   = get<T, Which>();
+      void * pNext = lhs.pNext;
+      lhs          = rhs;
+      lhs.pNext    = pNext;
+	    return *this;
+    }
+
     template <typename ClassType, size_t Which = 0>
     typename std::enable_if<
       std::is_same<ClassType, typename std::tuple_element<0, std::tuple<ChainElements...>>::type>::value &&

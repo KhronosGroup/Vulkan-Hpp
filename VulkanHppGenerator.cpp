096e555640308efa86605dd4e51d74d63b11a6f0
Janeczko Jakub <kuba@ev1.pl>
2024-07-15 12:39:19 +0200

    Add alias bitmasks to module interface (#1917)
    * add alias bitmasks to c++ module
    
    * generate code
    
    * format

--- VulkanHppGenerator.cpp
+++ VulkanHppGenerator.cpp
@@ -5649,6 +5649,10 @@ std::string VulkanHppGenerator::generateCppModuleEnumUsings() const
                bitmaskIt != m_bitmasks.end() )
           {
             localUsings += replaceWithMap( usingTemplate, { { "enumName", stripPrefix( bitmaskIt->first, "Vk" ) } } );
+            for ( auto const & alias : bitmaskIt->second.aliases )
+            {
+              localUsings += replaceWithMap( usingTemplate, { { "enumName", stripPrefix( alias.first, "Vk" ) } } );
+            }
           }
         }
       }

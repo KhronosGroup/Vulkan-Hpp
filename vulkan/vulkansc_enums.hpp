469fd9d24f00ca179d87005f4e69b2d2558c740e
asuessenbach <asuessenbach@nvidia.com>
2024-08-07 11:24:48 +0200

    Add IWYU pragmas to let code-completers use vulkan.hpp.

--- vulkan/vulkansc_enums.hpp
+++ vulkan/vulkansc_enums.hpp
@@ -8,6 +8,9 @@
 #ifndef VULKAN_ENUMS_HPP
 #define VULKAN_ENUMS_HPP
 
+// include-what-you-use: make sure, vulkan.hpp is used by code-completers
+// IWYU pragma: private; include "vulkan.hpp"
+
 namespace VULKAN_HPP_NAMESPACE
 {
   template <typename FlagBitsType>
--- vulkan/vulkansc_funcs.hpp
+++ vulkan/vulkansc_funcs.hpp
@@ -8,6 +8,9 @@
 #ifndef VULKAN_FUNCS_HPP
 #define VULKAN_FUNCS_HPP
 
+// include-what-you-use: make sure, vulkan.hpp is used by code-completers
+// IWYU pragma: private; include "vulkan.hpp"
+
 namespace VULKAN_HPP_NAMESPACE
 {
 

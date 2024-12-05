3691a24dc5e3589da87eca29383f1dd1b7de5b36
asuessenbach <asuessenbach@nvidia.com>
2024-12-05 08:27:09 +0100

    Export detail::getDispatchLoaderStatic in vulkan.cppm

--- VulkanHppGenerator.cpp
+++ VulkanHppGenerator.cpp
@@ -5801,6 +5801,7 @@ std::string VulkanHppGenerator::generateCppModuleUsings() const
     using VULKAN_HPP_NAMESPACE::detail::DispatchLoaderDynamic;
 #if !defined( VK_NO_PROTOTYPES )
     using VULKAN_HPP_NAMESPACE::detail::DispatchLoaderStatic;
+    using VULKAN_HPP_NAMESPACE::detail::getDispatchLoaderStatic;
 #endif /*VK_NO_PROTOTYPES*/
   }
 )" };

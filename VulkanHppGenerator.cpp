8672aa4aaa3fe97b27f55ab728f3e41f331a72a3
LEE KYOUNGHEON <stripe2933@gmail.com>
2025-02-05 16:47:13 +0900

    Export std::{tuple_size,tuple_element} for vk::StructureChain. (#2060)
    * Export std::{tuple_size,tuple_element} for vk::StructureChain.
    
    * Add separator

--- VulkanHppGenerator.cpp
+++ VulkanHppGenerator.cpp
@@ -826,6 +826,15 @@ export namespace VULKAN_HPP_NAMESPACE
 export namespace std
 {
   ${hashSpecializations}
+
+  //===============================================
+  //=== Required exports for vk::StructureChain ===
+  //===============================================
+
+#if !defined( VULKAN_HPP_DISABLE_ENHANCED_MODE )
+  using std::tuple_size;
+  using std::tuple_element;
+#endif
 }
 
 // This VkFlags type is used as part of a bitfield in some structure.

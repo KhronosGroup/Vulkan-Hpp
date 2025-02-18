85036c964cae9ca1d355faebbfadedef1a25bf76
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2025-02-18 08:20:17 +0100

    C++20 module: Hide to_string behind VULKAN_HPP_NO_TO_STRING (#2077)
    * hide to_string behind VULKAN_HPP_NO_TO_STRING
    
    * adjusted generated cppm

--- snippets/CppModuleFileTemplate.hpp
+++ snippets/CppModuleFileTemplate.hpp
@@ -17,7 +17,9 @@ module;
 #include <vulkan/${api}_hash.hpp>
 #include <vulkan/${api}_raii.hpp>
 #include <vulkan/${api}_shared.hpp>
+#ifndef VULKAN_HPP_NO_TO_STRING
 #include <vulkan/${api}_to_string.hpp>
+#endif
 
 export module ${api}_hpp;
 

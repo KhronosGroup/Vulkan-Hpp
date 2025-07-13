9afa0c3737da0cc172943bc593355506876172f3
GitHub <noreply@github.com>
2025-07-13 00:18:15 +0000

    Update Vulkan-Headers to v1.4.322

--- vulkan/vulkan.hpp
+++ vulkan/vulkan.hpp
@@ -56,7 +56,7 @@ extern "C" __declspec( dllimport ) FARPROC __stdcall GetProcAddress( HINSTANCE h
 #  endif
 #endif
 
-static_assert( VK_HEADER_VERSION == 321, "Wrong VK_HEADER_VERSION!" );
+static_assert( VK_HEADER_VERSION == 322, "Wrong VK_HEADER_VERSION!" );
 
 // <tuple> includes <sys/sysmacros.h> through some other header
 // this results in major(x) being resolved to gnu_dev_major(x)

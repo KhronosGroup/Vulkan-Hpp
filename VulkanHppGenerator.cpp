e6d0135c572124ecbb3aeb5a02f2a8863550aa7a
Andreas Süßenbach <asuessenbach@nvidia.com>
2025-07-21 15:11:19 +0200

    Improved error check on exports of commands. (#2225)

--- VulkanHppGenerator.cpp
+++ VulkanHppGenerator.cpp
@@ -14797,8 +14797,9 @@ void VulkanHppGenerator::readFeatureRequire( tinyxml2::XMLElement const * elemen
       checkForError( ( std::stoi( m_version ) < 319 ) || !commandIt->second.exports.empty(),
                      commandIt->second.xmlLine,
                      "command <" + commandIt->first + "> is required by feature <" + featureData.name + "> but is not marked as exported" );
-      checkForError( std::ranges::any_of( commandIt->second.exports,
-                                          [&featureData]( auto const & api ) { return std::ranges::find( featureData.api, api ) != featureData.api.end(); } ),
+      checkForError( commandIt->second.exports.empty() ||
+                       std::ranges::any_of( commandIt->second.exports,
+                                            [&featureData]( auto const & api ) { return std::ranges::find( featureData.api, api ) != featureData.api.end(); } ),
                      commandIt->second.xmlLine,
                      "command <" + commandIt->first + "> is required by feature <" + featureData.name + "> but is not exported for the feature's api" );
     }

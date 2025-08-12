9ae7e9060114af22c94a3bec59e52ed1db30e9f9
asuessenbach <asuessenbach@nvidia.com>
2025-08-12 12:53:38 +0200

    Refactor file generation functions

---
+++ snippets/StaticAssertionsHppTemplate.hpp
@@ -0,0 +1,13 @@
+${licenseHeader}
+
+#ifndef VULKAN_STATIC_ASSERTIONS_HPP
+#  define VULKAN_STATIC_ASSERTIONS_HPP
+
+#include <vulkan/${api}.hpp>
+
+//=========================
+//=== static_assertions ===
+//=========================
+
+${staticAssertions}
+#endif

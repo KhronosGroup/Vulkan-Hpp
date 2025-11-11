11e933f2528ec65b0b5d6a9970dc2b4dcac8bed0
asuessenbach <asuessenbach@nvidia.com>
2025-11-11 09:35:00 +0100

    Fix wrongly generated interface of vk::raii::Device::getFaultInfoEXT.

--- vulkan/vulkan_funcs.hpp
+++ vulkan/vulkan_funcs.hpp
@@ -24339,6 +24339,7 @@ namespace VULKAN_HPP_NAMESPACE
     return static_cast<Result>( d.vkGetDeviceFaultInfoEXT(
       static_cast<VkDevice>( m_device ), reinterpret_cast<VkDeviceFaultCountsEXT *>( pFaultCounts ), reinterpret_cast<VkDeviceFaultInfoEXT *>( pFaultInfo ) ) );
   }
+
 #if defined( VK_USE_PLATFORM_WIN32_KHR )
   //=== VK_NV_acquire_winrt_display ===
 

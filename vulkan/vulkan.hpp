587c9483b5e3bbed2de1e34e01a67bdc71c41fa7
Andreas Süßenbach <asuessenbach@nvidia.com>
2025-12-02 19:35:53 +0100

    BugFix: destroy potentially generated pipeline handles on partial creation failure (#2378)

--- vulkan/vulkan.hpp
+++ vulkan/vulkan.hpp
@@ -7596,6 +7596,36 @@ namespace VULKAN_HPP_NAMESPACE
       }
 #endif
     }
+
+    template <typename HandleType, typename AllocatorType, typename Dispatch>
+    VULKAN_HPP_INLINE void resultCheck( Result                                         result,
+                                        char const *                                   message,
+                                        std::initializer_list<Result>                  successCodes,
+                                        VkDevice                                       device,
+                                        std::vector<HandleType, AllocatorType> const & pipelines,
+                                        AllocationCallbacks const *                    pAllocator,
+                                        Dispatch const &                               d )
+    {
+#ifdef VULKAN_HPP_NO_EXCEPTIONS
+      ignore( result );  // just in case VULKAN_HPP_ASSERT_ON_RESULT is empty
+      ignore( message );
+      ignore( successCodes );  // just in case VULKAN_HPP_ASSERT_ON_RESULT is empty
+      ignore( device );
+      ignore( pipelines );
+      ignore( pAllocator );
+      ignore( d );
+      VULKAN_HPP_ASSERT_ON_RESULT( std::find( successCodes.begin(), successCodes.end(), result ) != successCodes.end() );
+#else
+      if ( std::find( successCodes.begin(), successCodes.end(), result ) == successCodes.end() )
+      {
+        for ( HandleType pipeline : pipelines )
+        {
+          d.vkDestroyPipeline( device, static_cast<VkPipeline>( pipeline ), reinterpret_cast<VkAllocationCallbacks const *>( pAllocator ) );
+        }
+        throwResultException( result, message );
+      }
+#endif
+    }
   }  // namespace detail
 
   //===========================

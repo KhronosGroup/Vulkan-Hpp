4a1e6df92b32d4ccc0ca925cb548fbeae6e8299d
Matt Guerrette <direct3Dtutorials@gmail.com>
2025-12-15 10:12:19 -0500

    Re-introduce support for std::expected, fixes issue #2375

--- vulkan/vulkan.hpp
+++ vulkan/vulkan.hpp
@@ -7490,7 +7490,11 @@ namespace VULKAN_HPP_NAMESPACE
   struct ResultValueType
   {
 #ifdef VULKAN_HPP_NO_EXCEPTIONS
+#ifdef VULKAN_HPP_EXPECTED
+    using type = VULKAN_HPP_EXPECTED<T, Result>;
+#else
     using type = ResultValue<T>;
+#endif
 #else
     using type = T;
 #endif
@@ -7500,7 +7504,11 @@ namespace VULKAN_HPP_NAMESPACE
   struct ResultValueType<void>
   {
 #ifdef VULKAN_HPP_NO_EXCEPTIONS
+#ifdef VULKAN_HPP_EXPECTED
+    using type = VULKAN_HPP_EXPECTED<void, Result>;
+#else
     using type = Result;
+#endif
 #else
     using type = void;
 #endif
@@ -7517,7 +7525,11 @@ namespace VULKAN_HPP_NAMESPACE
     VULKAN_HPP_INLINE typename ResultValueType<void>::type createResultValueType( Result result )
     {
 #if defined( VULKAN_HPP_NO_EXCEPTIONS )
+#ifdef VULKAN_HPP_UNEXPECTED
+      return VULKAN_HPP_UNEXPECTED(result);
+#else
       return result;
+#endif
 #else
       ignore( result );
 #endif
@@ -7527,7 +7539,15 @@ namespace VULKAN_HPP_NAMESPACE
     VULKAN_HPP_INLINE typename ResultValueType<T>::type createResultValueType( Result result, T & data )
     {
 #if defined( VULKAN_HPP_NO_EXCEPTIONS )
+#ifdef VULKAN_HPP_EXPECTED
+      if (result == Result::eSuccess)
+      {
+          return data;
+      }
+      return VULKAN_HPP_UNEXPECTED(result);
+#else
       return ResultValue<T>( result, data );
+#endif
 #else
       ignore( result );
       return data;
@@ -7538,7 +7558,16 @@ namespace VULKAN_HPP_NAMESPACE
     VULKAN_HPP_INLINE typename ResultValueType<T>::type createResultValueType( Result result, T && data )
     {
 #if defined( VULKAN_HPP_NO_EXCEPTIONS )
+#ifdef VULKAN_HPP_EXPECTED
+      if (result == Result::eSuccess)
+      {
+          return std::move(data);
+      }
+      return VULKAN_HPP_UNEXPECTED(result);
+#else
       return ResultValue<T>( result, std::move( data ) );
+#endif
+
 #else
       ignore( result );
       return std::move( data );

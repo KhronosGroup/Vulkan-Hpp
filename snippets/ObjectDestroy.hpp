396096255f032bbd3ca4bdafd26556cba583c67c
Andreas Süßenbach <asuessenbach@nvidia.com>
2026-02-24 08:52:43 +0100

    Add missing UniqueHandleTraits for handles without destroy function. (#2500)

--- snippets/ObjectDestroy.hpp
+++ snippets/ObjectDestroy.hpp
@@ -79,3 +79,45 @@ private:
   Optional<const AllocationCallbacks> m_allocationCallbacks = nullptr;
   Dispatch const *                    m_dispatch            = nullptr;
 };
+
+template <typename OwnerType, typename Dispatch>
+class DummyDestroy
+{
+public:
+  DummyDestroy() = default;
+
+  DummyDestroy( OwnerType                           owner,
+                Optional<const AllocationCallbacks> allocationCallbacks VULKAN_HPP_DEFAULT_ASSIGNMENT( nullptr ),
+                Dispatch const & dispatch           VULKAN_HPP_DEFAULT_DISPATCHER_ASSIGNMENT ) VULKAN_HPP_NOEXCEPT
+    : m_owner( owner )
+    , m_allocationCallbacks( allocationCallbacks )
+    , m_dispatch( &dispatch )
+  {
+  }
+
+  OwnerType getOwner() const VULKAN_HPP_NOEXCEPT
+  {
+    return m_owner;
+  }
+
+  Optional<const AllocationCallbacks> getAllocator() const VULKAN_HPP_NOEXCEPT
+  {
+    return m_allocationCallbacks;
+  }
+
+  Dispatch const & getDispatch() const VULKAN_HPP_NOEXCEPT
+  {
+    return *m_dispatch;
+  }
+
+protected:
+  template <typename T>
+  void destroy( T /*t*/ ) VULKAN_HPP_NOEXCEPT
+  {
+  }
+
+private:
+  OwnerType                           m_owner               = {};
+  Optional<const AllocationCallbacks> m_allocationCallbacks = nullptr;
+  Dispatch const *                    m_dispatch            = nullptr;
+};

5e8fe7afb4b138a16aafc54e04513a70f0ed0da3
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2026-05-13 10:30:18 +0200

    update test licenses

--- tests/NoExceptions/NoExceptions.cpp
+++ tests/NoExceptions/NoExceptions.cpp
@@ -1,20 +1,9 @@
-// Copyright(c) 2018, NVIDIA CORPORATION. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// VulkanHpp Samples : NoExceptions
-//                     Compile test with VULKAN_HPP_NO_EXCEPTIONS set
-//                     Note: this is _no_ functional test!! Don't ever code this way!!
+// SPDX-FileCopyrightText: 2018-2026 NVIDIA CORPORATION
+// SPDX-License-Identifier: Apache-2.0
+
+// VulkanHpp Tests : NoExceptions
+//                   Compile test with VULKAN_HPP_NO_EXCEPTIONS set
+//                   Note: this is _no_ functional test!! Don't ever code this way!!
 
 #if !defined( VULKAN_HPP_DISPATCH_LOADER_DYNAMIC )
 #  define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
--- tests/NoExceptionsRAII/NoExceptionsRAII.cpp
+++ tests/NoExceptionsRAII/NoExceptionsRAII.cpp
@@ -1,20 +1,9 @@
-// Copyright(c) 2018, NVIDIA CORPORATION. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// VulkanHpp Samples : NoExceptionsRAII
-//                     Compile test with VULKAN_HPP_NO_EXCEPTIONS set and using raii-classes
-//                     Note: this is _no_ functional test!! Don't ever code this way!!
+// SPDX-FileCopyrightText: 2018-2026 NVIDIA CORPORATION
+// SPDX-License-Identifier: Apache-2.0
+
+// VulkanHpp Tests : NoExceptionsRAII
+//                   Compile test with VULKAN_HPP_NO_EXCEPTIONS set and using raii-classes
+//                   Note: this is _no_ functional test!! Don't ever code this way!!
 
 #include "../test_macros.hpp"
 #ifdef VULKAN_HPP_USE_CXX_MODULE

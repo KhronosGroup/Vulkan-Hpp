9aad9bf9fe1b7482fab310dd20b8b4835315e381
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2026-06-08 21:04:38 +0200

    reformat vkGetDeviceProcAddr filter for device command assignment

--- generator/VulkanHppGenerator.cpp
+++ generator/VulkanHppGenerator.cpp
@@ -5888,7 +5888,7 @@ std::string VulkanHppGenerator::generateDispatchLoaderDynamicDeviceCommandAssign
   forEachRequiredCommand( requireData,
                           [&]( NameLine const & command, auto const & commandData )
                           {
-                            if ( !listedCommands.contains( command.name ) && command.name.compare( "vkGetDeviceProcAddr" ) && !commandData.second.handle.empty() && isDeviceCommand( commandData.second ) )
+                            if ( !listedCommands.contains( command.name ) && ( command.name != "vkGetDeviceProcAddr" ) && !commandData.second.handle.empty() && isDeviceCommand( commandData.second ) )
                             {
                               deviceCommandAssignments += generateDispatchLoaderDynamicCommandAssignment( command.name, commandData.first, "device" );
                             }

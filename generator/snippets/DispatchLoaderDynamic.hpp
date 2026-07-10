826e0263b5cac9bd9de03e9378db5f52423b8a08
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2026-07-10 19:32:03 +0200

    removed optional params from full DLD ctor

--- generator/snippets/DispatchLoaderDynamic.hpp
+++ generator/snippets/DispatchLoaderDynamic.hpp
@@ -27,8 +27,8 @@ public:
     // This interface does not require a linked vulkan library.
     DispatchLoaderDynamic( VkInstance                instance,
                             PFN_vkGetInstanceProcAddr getInstanceProcAddr,
-                            VkDevice                  device            = {},
-                            PFN_vkGetDeviceProcAddr   getDeviceProcAddr = nullptr ) VULKAN_HPP_NOEXCEPT
+                            VkDevice                  device,
+                            PFN_vkGetDeviceProcAddr   getDeviceProcAddr) VULKAN_HPP_NOEXCEPT
     {
         init( instance, getInstanceProcAddr, device, getDeviceProcAddr );
     }

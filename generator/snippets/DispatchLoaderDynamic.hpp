27b8b3f42d22a33d33c05a5ef78a4095c2e9b6f5
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2026-07-10 19:31:36 +0200

    added DLD ctor

--- generator/snippets/DispatchLoaderDynamic.hpp
+++ generator/snippets/DispatchLoaderDynamic.hpp
@@ -19,6 +19,13 @@ public:
 
     // This interface does not require a linked vulkan library.
     DispatchLoaderDynamic( VkInstance                instance,
+                            PFN_vkGetInstanceProcAddr getInstanceProcAddr) VULKAN_HPP_NOEXCEPT
+    {
+        init( instance, getInstanceProcAddr );
+    }
+
+    // This interface does not require a linked vulkan library.
+    DispatchLoaderDynamic( VkInstance                instance,
                             PFN_vkGetInstanceProcAddr getInstanceProcAddr,
                             VkDevice                  device            = {},
                             PFN_vkGetDeviceProcAddr   getDeviceProcAddr = nullptr ) VULKAN_HPP_NOEXCEPT

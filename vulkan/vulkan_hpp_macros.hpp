c4d2d58d466d8a9b520c348d24e2047367b49b2d
Andreas Süßenbach <asuessenbach@nvidia.com>
2026-08-20 14:43:56 +0200

    Start using concepts with C++20 and above. (#2665)

--- vulkan/vulkan_hpp_macros.hpp
+++ vulkan/vulkan_hpp_macros.hpp
@@ -339,6 +339,16 @@ VULKAN_HPP_COMPILE_WARNING( "This is a non-conforming implementation of C++ name
 #endif
 #define VULKAN_HPP_DEFAULT_DISPATCHER_ASSIGNMENT VULKAN_HPP_DEFAULT_ASSIGNMENT( VULKAN_HPP_DEFAULT_DISPATCHER )
 
+#if VULKAN_HPP_CPP_VERSION < 20
+#  define VULKAN_HPP_TEMPLATE_INTEGRAL    template <typename T, typename = typename std::enable_if<std::is_integral<T>::value>::type>
+#  define VULKAN_HPP_RAII_TEMPLATE_HANDLE template <typename T, typename std::enable_if<isVulkanRAIIHandleType<T>::value, bool>::type = 0>
+#else
+#  define VULKAN_HPP_TEMPLATE_INTEGRAL template <std::integral T>
+#  define VULKAN_HPP_RAII_TEMPLATE_HANDLE \
+    template <typename T>                 \
+    requires( isVulkanRAIIHandleType<T>::value )
+#endif
+
 #if !defined( VULKAN_HPP_EXPECTED ) && ( 23 <= VULKAN_HPP_CPP_VERSION ) && defined( __cpp_lib_expected ) && defined( VULKAN_HPP_USE_STD_EXPECTED )
 #  if !( defined( VULKAN_HPP_ENABLE_STD_MODULE ) && defined( VULKAN_HPP_STD_MODULE ) )
 #    include <expected>

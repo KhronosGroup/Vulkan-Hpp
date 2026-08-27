316ea736541ac4ab75fd4e4f37acb4595229b269
Christophe <christophe@lunarg.com>
2026-08-27 12:44:05 +0200

    Parse required properties

--- generator/VkXMLParser.hpp
+++ generator/VkXMLParser.hpp
@@ -449,6 +449,14 @@ struct MultiFeatureElement
   int                      xmlLine   = {};
 };
 
+struct PropertyElement
+{
+  std::string name      = {};
+  std::string structure = {};
+  std::string value     = {};
+  int         xmlLine   = {};
+};
+
 struct ExtensionRequire
 {
   std::string                              api      = {};
@@ -457,6 +465,7 @@ struct ExtensionRequire
   std::string                              depends  = {};
   std::vector<ExtensionRequireEnumVariant> enums    = {};
   std::vector<MultiFeatureElement>         features = {};
+  std::vector<PropertyElement>             properties = {};
   std::vector<NameElement>                 types    = {};
   int                                      xmlLine  = {};
 };
@@ -548,6 +557,7 @@ struct Require
   std::vector<ExtendEnumConstant> enumConstants = {};
   std::vector<ExtendEnumRegular>  enumRegulars  = {};
   std::vector<FeatureElement>     features      = {};
+  std::vector<PropertyElement>    properties    = {};
   std::vector<RequireType>        types         = {};
   int                             xmlLine       = {};
 };
